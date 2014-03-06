6e42715fed912f901cfb03561ae91ec306c48b39
tildeslash <info@tildeslash.com>
2014-03-06 11:01:20 +0100

    add back types include (required for openbsd)

--- src/protocols/radius.c
+++ src/protocols/radius.c
@@ -29,6 +29,10 @@
 #include <string.h>
 #endif
 
+#ifdef HAVE_SYS_TYPES_H
+#include <sys/types.h>
+#endif
+
 #ifdef HAVE_SYS_SOCKET_H
 #include <sys/socket.h>
 #endif
--- src/protocols/sip.c
+++ src/protocols/sip.c
@@ -31,6 +31,10 @@
 #include <string.h>
 #endif
 
+#ifdef HAVE_SYS_TYPES_H
+#include <sys/types.h>
+#endif
+
 #ifdef HAVE_SYS_SOCKET_H
 #include <sys/socket.h>
 #endif
