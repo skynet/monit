916d416d8cbca8259c454e260436769ab544b0d6
tildeslash <info@tildeslash.com>
2014-09-09 08:31:22 +0200

    save program output

--- src/validate.c
+++ src/validate.c
@@ -94,6 +94,7 @@
 // libmonit
 #include "system/Time.h"
 #include "io/File.h"
+#include "io/InputStream.h"
 
 /**
  *  Implementation of validation engine
@@ -109,6 +110,23 @@
 
 
 /* ----------------------------------------------------------------- Private */
+
+
+/**
+ * Read program output into stringbuffer. Limit the output to 2kB (if the program will have endless output, such as 'yes' utility, we have to stop at some point to not spin here forever)
+ */
+static void _programOutput(InputStream_T I, StringBuffer_T S) {
+        int n;
+        char buf[STRLEN];
+        InputStream_setTimeout(I, 0);
+        do {
+                n = InputStream_readBytes(I, buf, sizeof(buf) - 1);
+                if (n) {
+                        buf[n] = 0;
+                        StringBuffer_append(S, "%s", buf);
+                }
+        } while (n > 0 && StringBuffer_length(S) < 2048);
+}
 
 
 /**
@@ -1243,24 +1261,18 @@ int check_program(Service_T s) {
                         }
                 }
                 s->program->exitStatus = Process_exitStatus(P); // Save exit status for web-view display
+                // Save program output
+                StringBuffer_clear(s->program->output);
+                _programOutput(Process_getErrorStream(P), s->program->output);
+                _programOutput(Process_getInputStream(P), s->program->output);
                 // Evaluate program's exit status against our status checks.
                 /* TODO: Multiple checks we have now should be deprecated and removed - not useful because it
                  will alert on everything if != is used other than the match or if = is used, might report nothing on error. */
                 for (Status_T status = s->statuslist; status; status = status->next) {
                         if (Util_evalQExpression(status->operator, s->program->exitStatus, status->return_value)) {
-                                int n = 0;
-                                char buf[STRLEN + 1];
-                                // Read message from script
-                                if ((n = InputStream_readBytes(Process_getErrorStream(P), buf, STRLEN - 1)) <= 0)
-                                        n = InputStream_readBytes(Process_getInputStream(P), buf, STRLEN - 1);
-                                if (n > 0) {
-                                        buf[n] = 0;
-                                        Event_post(s, Event_Status, STATE_FAILED, status->action, "%s", buf);
-                                } else {
-                                        Event_post(s, Event_Status, STATE_FAILED, status->action, "'%s' failed with exit status (%d) -- no output from program", s->path, s->program->exitStatus);
-                                }
+                                Event_post(s, Event_Status, STATE_FAILED, status->action, "'%s' failed with exit status (%d) -- %s", s->path, s->program->exitStatus, StringBuffer_toString(s->program->output));
                         } else {
-                                DEBUG("'%s' status check succeeded\n", s->name);
+                                DEBUG("'%s' status check succeeded -- %s\n", s->name, StringBuffer_toString(s->program->output));
                                 Event_post(s, Event_Status, STATE_SUCCEEDED, status->action, "status succeeded");
                         }
                 }
