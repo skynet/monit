e0f5940b4759b2e17b2d71d99545347908175dbd
Tildeslash <git@tildeslash.com>
2014-09-14 22:49:05 +0200

    Fix #54 - Modified MySQL protocol test to simply connect to MySQL and read the server handshake packet. No anonymous login attempt is performed so the server does not log this as a failed login attempt if anonymous login is turned off (recommended).

--- src/protocols/mysql.c
+++ src/protocols/mysql.c
@@ -27,115 +27,69 @@
 #ifdef HAVE_STRING_H
 #include <string.h>
 #endif
-
 #include "protocol.h"
 
+
+/* ----------------------------------------------------------- Definitions */
+
+
+#define MYSQL_ERROR 0xff
+typedef struct {unsigned int len:24, seq:8; unsigned char *msg; unsigned char buf[STRLEN + 1];} mysql_packet_t;
+
+
+/* --------------------------------------------------------------- Private */
+
+
+static unsigned short B2(unsigned char *b) {
+        unsigned char x[3] = {0, b[1], b[0]};
+        return ntohs(*(unsigned short*)x);
+}
+
+
+static unsigned int B3(unsigned char *b) {
+        unsigned char x[4] = {0, b[2], b[1], b[0]};
+        return ntohl(*(unsigned int*)x);
+}
+
+
+static int _response(Socket_T socket, mysql_packet_t *pkt) {
+        memset(pkt, 0, sizeof *pkt);
+        if (socket_read(socket, pkt->buf, STRLEN) < 4) {
+                socket_setError(socket, "MYSQL: error receiving server response -- %s", STRERROR);
+                return FALSE;
+        }
+        pkt->len = B3(pkt->buf);
+        pkt->seq = pkt->buf[3];
+        pkt->msg = pkt->buf + 4;
+        if (*pkt->msg == MYSQL_ERROR) {
+                unsigned short code = B2(pkt->msg + 1);
+                unsigned char *err = pkt->msg + 9;
+                socket_setError(socket, "MYSQL: server returned error code %d -- %s", code, err);
+                return FALSE;
+        }
+        return TRUE;
+}
+
+
+/* ---------------------------------------------------------------- Public */
+
+
 /**
- *  Simple MySQL test.
+ * Simple MySQL test. Connect to MySQL and read Server Handshake Packet.
+ * If we can read the packet and it is not an error packet we assume the 
+ * server is up and working.
  *
- *  In the case that the anonymous login is possible,
- *  we will perform MySQL ping. If authentication failed
- *  we suppose the anonymous login is denied and we will
- *  return success, because the server at least performed
- *  authentication => it seems it works.
- *
- *  @file
+ *  @see http://dev.mysql.com/doc/internals/en/client-server-protocol.html
  */
 int check_mysql(Socket_T socket) {
-
-
-  unsigned char buf[STRLEN];
-
-  unsigned char requestLogin[39] = {
-    0x23, 0x00, 0x00,       // packet_length, 3 bytes
-    0x01,                   // packet_number, 1 byte
-    0x00, 0xa2, 0x00, 0x00, // client_flags, 4 bytes (do+auth 4.1, transact)
-    0x00, 0x00, 0x00, 0x40, // max_packet_size, 4 bytes
-    0x08,                   // charset_number (latin1), 1 byte
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // filler, 23 bytes
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00,                   // username
-    0x00,                   // password
-    0x00
-  };
-
-  unsigned char requestPing[5] = {
-    0x01, 0x00, 0x00,       // packet_length, 3 bytes
-    0x00,                   // packet_number, 1 byte
-    0x0e                    // command ping (14), 1 byte
-  };
-
-  unsigned char responsePing[5] = {
-    0x07, 0x00, 0x00,       // packet_length, 3 bytes
-    0x01,                   // packet_number, 1 byte
-    0x00                    // affected_rows, 1 byte
-                            // remaining 4 bytes ignored
-  };
-
-  unsigned char requestQuit[5] = {
-    0x01, 0x00, 0x00,       // packet_length, 3 bytes
-    0x00,                   // packet_number, 1 byte
-    0x01                    // command quit (1), 1 byte
-  };
-
-  ASSERT(socket);
-
-  if(!socket_readln(socket, (char *)buf, sizeof(buf))) {
-    socket_setError(socket, "MYSQL: error receiving greeting -- %s", STRERROR);
-    return FALSE;
-  }
-
-  if(socket_write(socket, requestLogin, sizeof(requestLogin)) < 0) {
-    socket_setError(socket, "MYSQL: error sending login -- %s", STRERROR);
-    return FALSE;
-  }
-
-  /* read just first few bytes  which contains enough information */
-  errno = 0;
-  if(socket_read(socket, buf, 7) <= 6) {
-    socket_setError(socket, "MYSQL: error receiving login response");
-    return FALSE;
-  }
-
-  /* Compare Packet Number: */
-  if(buf[3] != 0x02) {
-    socket_setError(socket, "MYSQL: invalid response packet number");
-    return FALSE;
-  }
-
-  /* Compare Response Code: */
-  if(buf[4] == 0x00) {
-    /* If OK, we are loged in and will perform MySQL ping */
-    if(socket_write(socket, (unsigned char *)requestPing, sizeof(requestPing)) < 0) {
-      socket_setError(socket, "MYSQL: error sending ping -- %s", STRERROR);
-      return FALSE;
-    }
-
-    if(socket_read(socket, buf, sizeof(responsePing)) <= 0) {
-      socket_setError(socket, "MYSQL: error receiving ping response -- %s", STRERROR);
-      return FALSE;
-    }
-
-    if(memcmp((unsigned char *)buf,
-                (unsigned char *)responsePing, sizeof(responsePing))) {
-      socket_setError(socket, "MYSQL: ping failed");
-      return FALSE;
-    }
-
-    if(socket_write(socket, (unsigned char *)requestQuit, sizeof(requestQuit)) < 0) {
-      socket_setError(socket, "MYSQL: error sending quit -- %s", STRERROR);
-      return FALSE;
-    }
-
-    return TRUE;
-  } else if((buf[4] == 0xFF) && ((buf[5] == 0x15 && buf[6] == 0x04) || (buf[5] == 0xE3 && buf[6] == 0x04) || (buf[5] == 0x13 && buf[6] == 0x04))) {
-    /* If access denied (1045) or server requires newer authentication protocol (1251) or bad handshake (1043) return success immediately */
-    return TRUE;
-  }
-
-  socket_setError(socket, "MYSQL: login failed (error code %d)", buf[6] * 256 + buf[5]);
-
-  return FALSE;
+        ASSERT(socket);
+        mysql_packet_t pkt;
+        if (_response(socket, &pkt)) {
+                unsigned short protocol_version = pkt.msg[0];
+                unsigned char *server_version = pkt.msg + 1;
+                DEBUG("MySQL: Protocol: %d, Server Version: %s\n", protocol_version, server_version);
+                return TRUE;
+        }
+        return FALSE;
 }
 
