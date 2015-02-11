2ee641babe69869d8f59d48190985a826a5518de
tildeslash <info@tildeslash.com>
2015-02-11 13:46:58 +0100

    log invalid state

--- src/event.c
+++ src/event.c
@@ -395,10 +395,9 @@ const char *Event_get_description(Event_T E) {
  * @return An action id
  */
 Action_Type Event_get_action(Event_T E) {
-        Action_T A = NULL;
-
         ASSERT(E);
 
+        Action_T A = NULL;
         switch (E->state) {
                 case State_Succeeded:
                 case State_ChangedNot:
@@ -410,15 +409,10 @@ Action_Type Event_get_action(Event_T E) {
                         A = E->action->failed;
                         break;
                 default:
-                        break;
+                        LogError("Invalid event state: %d\n", E->state);
+                        return Action_Ignored;
         }
-
-        if (! A)
-                return Action_Ignored;
-
-        /* In the case of passive mode we replace the description of start, stop
-         * or restart action for alert action, because these actions are passive in
-         * this mode */
+        /* In the case of passive mode we replace the description of start, stop or restart action for alert action, because these actions are passive in this mode */
         return (E->mode == Monitor_Passive && ((A->id == Action_Start) || (A->id == Action_Stop) || (A->id == Action_Restart))) ? Action_Alert : A->id;
 }
 
