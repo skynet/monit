50323874d4e48e030e184737d35cd0ee6d0357b2
Jan-Henrik Haukeland <hauk@hauk.local>
2015-08-27 22:57:32 +0200

    Simplify how Host header is set

--- src/protocols/http.c
+++ src/protocols/http.c
@@ -258,24 +258,21 @@ void check_http(Socket_T socket) {
         Port_T P = Socket_getPort(socket);
         ASSERT(P);
 
-        const char *hostheader = _findHeaderIn(P->parameters.http.headers, "Host");
         StringBuffer_T sb = StringBuffer_create(168);
         StringBuffer_append(sb,
                             "GET %s HTTP/1.1\r\n"
-                            "Host: %s\r\n"
                             "Accept: */*\r\n"
                             "%s",
                             P->parameters.http.request ? P->parameters.http.request : "/",
-                            hostheader ? hostheader : Util_getHTTPHostHeader(socket, (char[STRLEN]){0}, STRLEN),
                             get_auth_header(P, (char[STRLEN]){0}, STRLEN));
+        if (! _findHeaderIn(P->parameters.http.headers, "Host"))
+                StringBuffer_append(sb, "Host: %s\r\n", Util_getHTTPHostHeader(socket, (char[STRLEN]){}, STRLEN));
         if (! _findHeaderIn(P->parameters.http.headers, "User-Agent"))
                 StringBuffer_append(sb, "User-Agent: Monit/%s\r\n", VERSION);
         // Add headers if we have them
         if (P->parameters.http.headers) {
                 for (list_t p = P->parameters.http.headers->head; p; p = p->next) {
                         char *header = p->e;
-                        if (Str_startsWith(header, "Host")) // Already set contrived above
-                                continue;
                         StringBuffer_append(sb, "%s\r\n", header);
                 }
         }
